9bb09f0412abbba1b065c485faa24605a001d029
AnnLiz22 <bielakam@gmail.com>
2024-11-16 23:44:46 +0100

    app class

---
+++ src/main/java/com/lietz/demo/DemoReservationApplication.java
@@ -0,0 +1,14 @@
+package com.lietz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReservationApplication.class, args);
+		System.out.println("hello there");
+	}
+
+}
