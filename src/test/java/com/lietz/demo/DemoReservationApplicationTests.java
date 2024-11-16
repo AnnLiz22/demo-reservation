871e60495a975285e14aa30cf7d1d178ebdc63c2
AnnLiz22 <bielakam@gmail.com>
2024-11-16 23:46:03 +0100

    basic test class

---
+++ src/test/java/com/lietz/demo/DemoReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lietz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
