798e1fc51729472e3dcb98023ce45c71ba875c37
AnnLiz22 <bielakam@gmail.com>
2024-12-09 11:50:12 +0100

    prepared accommodation controller

---
+++ src/main/java/com/lietz/demo/controller/AccommodationController.java
@@ -0,0 +1,13 @@
+package com.lietz.demo.controller;
+
+import com.lietz.demo.service.AccommodationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accommodations")
+@RequiredArgsConstructor
+public class AccommodationController {
+  private AccommodationService accommodationService;
+}
