32962ebf089f550a25b1576382bed89cab3f0e48
AnnLiz22 <bielakam@gmail.com>
2024-12-09 11:49:26 +0100

    added accommodation repository

---
+++ src/main/java/com/lietz/demo/repository/AccommodationRepository.java
@@ -0,0 +1,9 @@
+package com.lietz.demo.repository;
+
+import com.lietz.demo.model.Accommodation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+}
