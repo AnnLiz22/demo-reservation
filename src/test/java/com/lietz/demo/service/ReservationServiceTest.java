ece689513bf1cae00011bad13c24708618f660f9
AnnLiz22 <bielakam@gmail.com>
2024-12-13 23:44:46 +0100

    prepared class for testing reservation service

---
+++ src/test/java/com/lietz/demo/service/ReservationServiceTest.java
@@ -0,0 +1,52 @@
+package com.lietz.demo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.lietz.demo.model.Reservation;
+import com.lietz.demo.repository.ReservationRepository;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class ReservationServiceTest {
+
+  @Mock
+  ReservationRepository reservationRepository;
+
+  @InjectMocks
+  ReservationService reservationService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void getAllReservations() {
+    List <Reservation> reservations = List.of(
+        new Reservation()
+    );
+
+    when(reservationService.getAllReservations()).thenReturn(reservations);
+    reservationService.getAllReservations();
+    verify(reservationRepository, times(1)).findAll();
+  }
+
+  @Test
+  void createReservation() {
+  }
+
+  @Test
+  void getReservationById() {
+  }
+
+  @Test
+  void deleteReservation() {
+  }
+}
