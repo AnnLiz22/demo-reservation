a101c744f8b31119f1fced86c7e6006a6aa62103
AnnLiz22 <bielakam@gmail.com>
2024-12-19 20:49:30 +0100

    added testing methods

--- src/test/java/com/lietz/demo/service/ReservationServiceTest.java
+++ src/test/java/com/lietz/demo/service/ReservationServiceTest.java
@@ -1,17 +1,20 @@
 package com.lietz.demo.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.lietz.demo.model.Reservation;
 import com.lietz.demo.repository.ReservationRepository;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 class ReservationServiceTest {
@@ -22,6 +25,12 @@ class ReservationServiceTest {
   @InjectMocks
   ReservationService reservationService;
 
+  List <Reservation> reservations = List.of(
+      new Reservation(1L, 1L, 2L, LocalDate.of(2024,12,12), LocalDate.of(2024,12,14), "confirmed"),
+      new Reservation(2L, 2L, 4L, LocalDate.of(2024,11,12), LocalDate.of(2024,11,14), "confirmed"),
+      new Reservation(3L, 3L, 4L, LocalDate.of(2024,10,12), LocalDate.of(2024,10,14), "confirmed")
+  );
+
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
@@ -29,9 +38,6 @@ class ReservationServiceTest {
 
   @Test
   void shouldGetAllReservations() {
-    List <Reservation> reservations = List.of(
-        new Reservation()
-    );
 
     when(reservationRepository.findAll()).thenReturn(reservations);
     reservationService.getAllReservations();
@@ -39,14 +45,29 @@ class ReservationServiceTest {
   }
 
   @Test
-  void createReservation() {
+  void shouldCreateReservation() {
+    Reservation reservation = new Reservation();
+    when(reservationRepository.save(reservation)).thenReturn(reservation);
+
+    reservationService.createReservation(reservation);
+    Mockito.verify(reservationRepository, times(1)).save(reservation);
   }
 
   @Test
-  void getReservationById() {
+  void shouldGetReservationById() {
+    Reservation reservation1 = reservations.get(0);
+    when(reservationRepository.findById(1L)).thenReturn(Optional.ofNullable(reservation1));
+    reservationService.getReservationById(1L);
+    Assertions.assertEquals("confirmed", reservation1.getStatus());
+
+    Mockito.verify(reservationRepository, times(1)).findById(1L);
   }
 
   @Test
-  void deleteReservation() {
+  void shouldInvokeDeleteReservation() {
+    Reservation reservation1 = reservations.get(0);
+    reservationService.deleteReservation(reservation1.getId());
+
+    Mockito.verify(reservationRepository, times(1)).existsById(1L);
   }
 }
