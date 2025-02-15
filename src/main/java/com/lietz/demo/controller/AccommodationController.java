7eb18d78629249cafd6a110c3a47a4dd61c2c9e2
AnnLiz22 <bielakam@gmail.com>
2025-02-15 08:31:55 +0100

    modified controller for views

--- src/main/java/com/lietz/demo/controller/AccommodationController.java
+++ src/main/java/com/lietz/demo/controller/AccommodationController.java
@@ -4,31 +4,33 @@ import com.lietz.demo.model.Accommodation;
 import com.lietz.demo.service.AccommodationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/accommodations")
+@Controller
+@RequestMapping("/accommodations")
 @RequiredArgsConstructor
 public class AccommodationController {
   private final AccommodationService accommodationService;
 
   @GetMapping
-  public ResponseEntity<List<Accommodation>> getAllAccommodations(){
+  public String getAllAccommodations(Model model){
     List <Accommodation> accommodations = accommodationService.findAllAccommodations();
-    return ResponseEntity.ok(accommodations);
+    model.addAttribute("accommodations", accommodations);
+    return "accommodations/list";
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Accommodation> getAccommodation(@PathVariable Long id){
+  public String getAccommodation(@PathVariable Long id, Model model){
     try {
       Accommodation accommodation = accommodationService.findAccommodationById(id);
-      return ResponseEntity.ok(accommodation);
+      model.addAttribute("accommodation", accommodation);
+      return "accommodations/details";
     } catch (RuntimeException e){
-      return ResponseEntity.notFound().build();
+      return "error/404";
     }
   }
 }
